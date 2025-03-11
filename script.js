3a03ca948e4433e63e4496de2f01935dd773e46e
Pravesh532 <praveshpatel9569@gmail.com>
2025-03-11 15:08:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
-	if(s1.indexOf(s2)>0){
-		return s1.indexOf(s2)
-	}
-	
-	return -1 ;
+	 return str.toLowerCase().indexOf(subStr.toLowerCase());
 }
 
+
 // Please do not change the code below
-const s1 = promp("Enter s1:");
-const s2 = promp("Enter s2:");
-aler(indexOfIgnoreCase(s1, s2));
+const s1 = prompt("Enter s1:");
+const s2 = prompt("Enter s2:");
+alert(indexOfIgnoreCase(s1, s2));
